b2d812a854831fd79097ccadf39e42d7f442f593
Ellen Arvidsson <ellen@log-e.se>
2022-05-02 17:37:06 +0200

    ott-check and ott-preview

--- layers/+lang/ott/packages.el
+++ layers/+lang/ott/packages.el
@@ -3,5 +3,45 @@
 (defconst ott-packages
   '((ott-mode :location local)))
 
+(defun spacemacs-ott/ott-check ()
+  "Check current file for ott errors"
+  (interactive)
+  (shell-command (format
+                  "ott -colour false -i %s"
+                  (shell-quote-argument (buffer-file-name))))
+  )
+
+(defun spacemacs-ott/ott-preview ()
+  "Preview ott pdf"
+  (interactive)
+  (let* ((current (buffer-file-name))
+         (sans (file-name-sans-extension current))
+         (in  current)
+         (tex (concat sans ".tex"))
+         (out (concat sans ".pdf")))
+    (when (eq
+           0
+           (shell-command (format
+                           "ott -colour false -i %s -o %s"
+                           (shell-quote-argument in)
+                           (shell-quote-argument tex))))
+      (when (eq 0 (shell-command (format
+                                  "pdflatex %s"
+                                  (shell-quote-argument tex))))
+        (spacemacs-ott/ott-open-pdf out)))
+    )
+  )
+
+(defun spacemacs-ott/ott-open-pdf (file)
+  (async-shell-command (format "open %s" (shell-quote-argument file)))
+  )
+
 (defun ott/init-ott-mode ()
-  (use-package ott-mode))
+  (use-package ott-mode
+    :config
+    (progn
+      (spacemacs/set-leader-keys-for-major-mode 'ott-mode
+        "c" 'spacemacs-ott/ott-check
+        "p" 'spacemacs-ott/ott-preview))
+    )
+  )
